da788712fe379211d11372754817f55ca15a624b
Noah <noah231515@gmail.com>
2023-07-24 08:34:28 -0400

    bump version, add missing index

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -1,9 +1,14 @@
 /*
  * Public API Surface of core
  */
-
 export * from './lib/api';
 export * from './lib/auth';
 export * from './lib/base-input';
 export * from './lib/button';
+export * from './lib/constants';
+export * from './lib/directives';
+export * from './lib/guards';
 export * from './lib/input';
+export * from './lib/pipes';
+export * from './lib/services';
+export * from './lib/store';
