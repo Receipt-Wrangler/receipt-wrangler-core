a06b9edc0e49a665e9b4ee9f15bc33207380becf
Noah <noah231515@gmail.com>
2023-08-11 00:32:46 -0400

    move things, attempt to fix testing issues

--- projects/core/src/lib/guards/feature.guard.spec.ts
+++ projects/core/src/lib/guards/feature.guard.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { NgxsModule } from '@ngxs/store';
 import { FeatureGuard } from './feature.guard';
+import { FeatureConfigState } from '../store/feature-config.state';
 
 describe('FeatureGuard', () => {
   let guard: FeatureGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [NgxsModule.forRoot([])],
+      imports: [NgxsModule.forRoot([FeatureConfigState])],
     });
     guard = TestBed.inject(FeatureGuard);
   });
